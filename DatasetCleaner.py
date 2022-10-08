e255b6409ea5540a53bd130b2ca598d8e8f62211
JM-Elbambo <jm.elbambo27@gmail.com>
2022-10-08 16:33:26 +0800

    Add removal of stopwords in cleaning

--- DatasetCleaner.py
+++ DatasetCleaner.py
@@ -1,11 +1,22 @@
 import os
 import re
 import pandas as pd
+import numpy as np
+from nltk.corpus import stopwords
 
 
 class DatasetCleaner():
+	# Define stopwords
+	STOPWORDS_LIST = stopwords.words('english')
 
 	def clean_csv(input_filepath: str, output_filepath: str, columns: list):
+		"""Outputs a cleaned csv data containing the specified columns
+
+		Args:
+			input_filepath (str): path to the input csv file
+			output_filepath (str): path to the output csv file
+			columns (list): a list of strings containing the column names to keep
+		"""
 		# Load csv file
 		df = pd.read_csv(input_filepath)
 
@@ -16,21 +27,49 @@ class DatasetCleaner():
 		string_columns = (df.applymap(type) == str).all(0)
 
 		# Clean string columns
-		# Lowercase characters
-		# Remove non-alpha characters
-		# Remove leading and trailing whitespace
+		# df[df.columns[string_columns]] = df[df.columns[string_columns]].applymap(lambda x: \
+		# 	DatasetCleaner.remove_stopwords(DatasetCleaner.preprocess_text(x)))
+
 		df[df.columns[string_columns]] = df[df.columns[string_columns]].apply(lambda x: \
-			x.str.lower().str.replace('[^a-z ]', '').str.strip())
+			DatasetCleaner.remove_stopwords(DatasetCleaner.preprocess_text(x)))
 
 		# Remove rows with empty values
+		df.replace('', np.nan, inplace=True)
 		df.dropna(axis=0, inplace=True)
 
 		# Create output directory
-		os.makedirs(re.sub(r"[^\\]+\.csv$", '', output_filepath))
+		os.makedirs(re.sub(r"[^\\]+\.csv$", '', output_filepath), exist_ok=True)
 
 		# Export to csv
 		pd.DataFrame.to_csv(df, output_filepath, index=False)
+	
+	def preprocess_series(series: pd.Series):
+		"""Processes text in series to lowercase characters,
+		removes non-alpha characters,
+		removes multiple spaces, and
+		removes leading and trailing whitespaces
 
+		Args:
+			series (pandas.Series): series to process
 
-if  __name__ == "__main__":
-	DatasetCleaner.clean_csv(r"Data Sets\Raw\Training Set.csv", r"Data Sets\Processed\Training Set.csv", ['title', 'post', 'class_id'])
+		Returns:
+			pandas.Series: processed series
+		"""
+		series = series.str.lower()
+		series = series.str.replace("[^a-z ]", ' ', regex=True)
+		series = series.str.replace(" +", ' ', regex=True)
+		series = series.str.strip()
+		return series
+	
+	def remove_stopwords(series: pd.Series):
+		"""Removes stopwords from text in series
+
+		Args:
+			series (pandas.Series): series to process
+
+		Returns:
+			pandas.Series: processed series without stopwords
+		"""
+		rows = [[word for word in row if word not in DatasetCleaner.STOPWORDS_LIST] for row in series.str.split()]
+		rows = [' '.join(row) for row in rows]
+		return pd.Series(rows)
