25b6100c8ff9ff89b862f9dd55a7c3bb58f96be8
JM-Elbambo <jm.elbambo27@gmail.com>
2022-11-03 14:08:39 +0800

    Update for gridsearch usage

--- Main.py
+++ Main.py
@@ -11,26 +11,26 @@ if __name__ == "__main__":
 	path_processed_test = r"Data Sets\Processed\Test Set.csv"
 
 	# Data cleaning
-	print("\n============================================================\n")
-	print("Cleaning:", path_raw_training)
-	DatasetCleaner.clean_csv(path_raw_training, path_processed_training, ['title', 'post', 'class_id'])
-	print("Cleaning:", path_raw_validation)
-	DatasetCleaner.clean_csv(path_raw_validation, path_processed_validation, ['title', 'post', 'class_id'])
-	print("Cleaning:", path_raw_test)
-	DatasetCleaner.clean_csv(path_raw_test, path_processed_test, ['title', 'post', 'class_id'])
+	# print("\n============================================================\n")
+	# print("Cleaning:", path_raw_training)
+	# DatasetCleaner.clean_csv(path_raw_training, path_processed_training, ['title', 'post', 'class_id'])
+	# print("Cleaning:", path_raw_validation)
+	# DatasetCleaner.clean_csv(path_raw_validation, path_processed_validation, ['title', 'post', 'class_id'])
+	# print("Cleaning:", path_raw_test)
+	# DatasetCleaner.clean_csv(path_raw_test, path_processed_test, ['title', 'post', 'class_id'])
 
 	# Train model
 	# print("\n============================================================\n")
 	# print("TRAINING PHASE")
-	# model = Model(["title", "post"], "class_id", ["ADHD", "Anxiety", "Bipolar", "Depression", "PTSD", "None"])
+	model = Model(["title", "post"], "class_id", ["ADHD", "Anxiety", "Bipolar", "Depression", "PTSD", "None"])
 	# model.train(path_processed_training)
 
-	# print("\n============================================================\n")
-	# print("HYPERPARAMETER TUNING")
-	# train_grid_search = model.perform_grid_search(path_processed_training)
-	# validation_grid_search = model.perform_grid_search(path_processed_validation)
-	# print("Train: " + str(train_grid_search))
-	# print("Validation: " + str(validation_grid_search))
+	print("\n============================================================\n")
+	print("HYPERPARAMETER TUNING")
+	train_grid_search = model.perform_grid_search(path_processed_training)
+	validation_grid_search = model.perform_grid_search(path_processed_validation)
+	print("Train: " + str(train_grid_search))
+	print("Validation: " + str(validation_grid_search))
 	# model.graph_hyperparameter_tuning(path_processed_training, path_processed_validation)
 
 	# Test model
